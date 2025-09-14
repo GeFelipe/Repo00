78d23f70e242f00506c2afa7ea9da01b543b0c11
Andrés <afelipe.26@hotmail.com>
2025-09-14 16:43:51 -0500

    14-09 16:43

---
+++ main.py
@@ -0,0 +1,17 @@
+import datetime;
+import decimal;
+#Se crea clase
+class Estados:
+    id: int = 0;
+    nombre: str = None;
+
+"""
+    int(int-long)
+    float, decimal, complex
+    datetime (datetime.datetime.now())
+    bool (True, False)
+"""
+
+#Constructor
+estado = Estados();
+print("Hello everybody");
